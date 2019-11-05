05180f296e90db501c99e83cc70f0913e87d0ed2
eccelyanggraeni <eccelyanggraeni1124@gmail.com>
2019-11-05 10:46:24 +0700

    Version 3

--- main.java
+++ main.java
@@ -6,8 +6,8 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Version2");
-		System.out.println("Hello world!2");
+		System.out.println("Version3");
+		System.out.println("Hello world!3");
 	}
 
 }
