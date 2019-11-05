e508a2c2612d1d61abc7bd55c0239a387f97551d
eccelyanggraeni <eccelyanggraeni1124@gmail.com>
2019-11-05 10:00:55 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+package pegadaianGIT;
+
+import java.io.*;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Version1");
+		System.out.println("Hello world!");
+	}
+
+}
